c28edfb0b3e09c2a2f9b8d350e5e6a5b9b435cdf
pachoyan <pauchorroyanguas@gmail.com>
2017-02-14 00:02:44 +0100

    Exercise

--- 7-An Introduction to Components/Exercise 1/src/App.vue
+++ 7-An Introduction to Components/Exercise 1/src/App.vue
@@ -1,39 +1,27 @@
 <template>
     <div class="container">
         <div class="row">
-            <div class="col-xs-12">
-                <header>
-                    <h1>Server Status</h1>
-                </header>
-            </div>
+            <header-server></header-server>
         </div>
         <hr>
-        <div class="row">
-            <div class="col-xs-12 col-sm-6">
-                <ul class="list-group">
-                    <li
-                            class="list-group-item"
-                            v-for="index in 5">
-                        Server #{{ index }}
-                    </li>
-                </ul>
-            </div>
-            <div class="col-xs-12 col-sm-6">
-                <p>Server Details are currently not updated</p>
-            </div>
-        </div>
+        <app-server-detail-list></app-server-detail-list>
         <hr>
-        <div class="row">
-            <div class="col-xs-12">
-                <footer>
-                    <p>All Servers are managed here</p>
-                </footer>
-            </div>
-        </div>
+        <footer-server></footer-server>
     </div>
 </template>
 
 <script>
+import ServerDetailList  from './ServerDetailList.vue';
+import Footer  from './Footer.vue';
+import Header  from './Header.vue';
+
+  export default {
+    components : {
+      'app-server-detail-list' : ServerDetailList,
+      'footer-server' : Footer,
+      'header-server' : Header
+    }
+  }
 </script>
 
 <style>
---
+++ 7-An Introduction to Components/Exercise 1/src/ServerDetailList.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+     <div class="row">
+            <div class="col-xs-12 col-sm-6">
+                <app-server-list></app-server-list>
+            </div>
+            <div class="col-xs-12 col-sm-6">
+                <p>Server Details are currently not updated</p>
+            </div>
+        </div>
+  </div>
+</template>
+
+<script>
+import ServerList  from './ServerList.vue';
+
+  export default {
+    components : {
+      'app-server-list' : ServerList
+    }
+  }
+</script>
