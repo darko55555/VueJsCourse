40ad2f213f1fbb933af5d603f013224408d6140e
pachoyan <pauchorroyanguas@gmail.com>
2017-02-14 23:01:59 +0100

    Components

---
+++ 8-Communicating between components/6-Communicating with Callback Functions/src/components/UserDetail.vue
@@ -0,0 +1,34 @@
+<template>
+    <div class="component">
+        <h3>You may view the User Details here</h3>
+        <p>Many Details</p>
+        <p>User Name :  {{ switchName() }} </p>
+        <button @click="resetName">Reset name</button>
+    </div>
+</template>
+
+<script>
+  export default {
+    props: {
+        myName : {
+            type : String
+        }
+    },
+    methods: {
+        switchName() {
+            return this.myName.split("").reverse().join("");
+        },
+        resetName () {
+            this.myName = 'Pau'; //this works because is a object not a primitive and updates the reference
+            this.$emit('nameWasReset', this.myName);
+        }
+
+    }
+  }
+</script>
+
+<style scoped>
+    div {
+        background-color: lightcoral;
+    }
+</style>
