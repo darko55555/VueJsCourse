f6fde096d23932eb6115642eedb6f031894dcc9d
pachoyan <pauchorroyanguas@gmail.com>
2017-02-14 22:50:57 +0100

    Components

---
+++ 8-Communicating between components/4-Validating props/src/components/UserDetail.vue
@@ -0,0 +1,44 @@
+<template>
+    <div class="component">
+        <h3>You may view the User Details here</h3>
+        <p>Many Details</p>
+        <p>User Name :  {{ switchName() }} </p>
+    </div>
+</template>
+
+<script>
+  export default {
+    props: {
+        myName : {
+            type : String
+            //
+            //Multiple types [String, Array]
+            //
+            //Validate if it is required
+            //required : true,
+            //Default value
+            //default : 'Pau'
+            //
+            //If it is an Object you can validate struct.
+            //Example:
+            //type: Object
+            //default : function {
+            //    return {
+            //      name : 'Max'
+            //    }
+            //}
+        }
+    },
+    methods: {
+        switchName() {
+            return this.myName.split("").reverse().join("");
+        }
+    }
+  }
+</script>
+
+<style scoped>
+    div {
+        background-color: lightcoral;
+    }
+</style>
