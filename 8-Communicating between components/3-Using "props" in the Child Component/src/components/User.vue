f67fee17e6d6fa8bd51b66c6c980daf5f4b523f2
pachoyan <pauchorroyanguas@gmail.com>
2017-02-14 22:34:15 +0100

    Communication

---
+++ 8-Communicating between components/3-Using "props" in the Child Component/src/components/User.vue
@@ -0,0 +1,45 @@
+<template>
+    <div class="component">
+        <h1>The User Component</h1>
+        <p>I'm an awesome User!</p>
+        <button @click="changeName">Change name</button>
+        <hr>
+        <div class="row">
+            <div class="col-xs-12 col-sm-6">
+                <app-user-detail :myName="name"></app-user-detail>
+            </div>
+            <div class="col-xs-12 col-sm-6">
+                <app-user-edit></app-user-edit>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+    import UserDetail from './UserDetail.vue';
+    import UserEdit from './UserEdit.vue';
+
+
+    export default {
+        components: {
+            appUserDetail: UserDetail,
+            appUserEdit: UserEdit
+        },
+        data : function() {
+            return {
+                name : 'Pau'
+            };
+        },
+        methods : {
+            changeName() {
+                this.name = 'Anna';
+            }
+        }
+    }
+</script>
+
+<style scoped>
+    div {
+        background-color: lightblue;
+    }
+</style>
