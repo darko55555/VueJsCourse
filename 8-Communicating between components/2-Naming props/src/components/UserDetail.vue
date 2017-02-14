ba6b30ada7689873834cef7b6b69cd467f78eb59
pachoyan <pauchorroyanguas@gmail.com>
2017-02-14 22:37:17 +0100

    Fix error

--- 8-Communicating between components/2-Naming props/src/components/UserDetail.vue
+++ 8-Communicating between components/2-Naming props/src/components/UserDetail.vue
@@ -2,7 +2,7 @@
     <div class="component">
         <h3>You may view the User Details here</h3>
         <p>Many Details</p>
-        <p>User Name :  {{ name }} </p>
+        <p>User Name :  {{ myName }} </p>
     </div>
 </template>
 
