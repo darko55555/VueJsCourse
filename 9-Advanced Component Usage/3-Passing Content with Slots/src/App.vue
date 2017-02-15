5e3ab3c2aeba51b0e60c50a49e9e0644e8b856ca
pachoyan <pauchorroyanguas@gmail.com>
2017-02-16 00:29:01 +0100

    Components

---
+++ 9-Advanced Component Usage/3-Passing Content with Slots/src/App.vue
@@ -0,0 +1,29 @@
+<template>
+    <div class="container">
+        <div class="row">
+            <div class="col-xs-12">
+              <!--This is an example which no works.
+              It this doesn0t works.
+              To pass html to the child . It will work in the next exercise with slots. -->
+               <app-quote >
+                  <h2>The Quote</h2>
+                  <p>A wonderful Quote</p>
+               </app-quote>
+
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+    import Quote from './components/Quote.vue'
+
+    export default {
+      components: {
+        appQuote : Quote
+      }
+    }
+</script>
+
+<style>
+</style>
---
+++ 9-Advanced Component Usage/4-How Slot Content gets Compiled and Styled/src/App.vue
@@ -0,0 +1,34 @@
+<template>
+    <div class="container">
+        <div class="row">
+            <div class="col-xs-12">
+              <!--This is an example which no works.
+              It this doesn0t works.
+              To pass html to the child . It will work in the next exercise with slots. -->
+               <app-quote >
+                  <h2>{{ quoteTitle }}</h2>
+                  <p>A wonderful Quote</p>
+               </app-quote>
+
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+    import Quote from './components/Quote.vue'
+
+    export default {
+      data : function () {
+        return {
+          quoteTitle : 'The Quote'
+        }
+      },
+      components: {
+        appQuote : Quote
+      }
+    }
+</script>
+
+<style>
+</style>
