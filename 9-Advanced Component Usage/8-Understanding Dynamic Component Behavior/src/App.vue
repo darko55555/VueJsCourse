98b54bf8c3bf499f020b8dc9df0f9d2ea44e927a
pachoyan <pauchorroyanguas@gmail.com>
2017-02-16 22:51:46 +0100

    Components lifecycle

---
+++ 9-Advanced Component Usage/8-Understanding Dynamic Component Behavior/src/App.vue
@@ -0,0 +1,44 @@
+<template>
+    <div class="container">
+        <div class="row">
+            <div class="col-xs-12">
+            <button @click="selectedComponent = 'appQuote'">Quote</button>
+            <button @click="selectedComponent = 'appAuthor'">Author</button>
+            <button @click="selectedComponent = 'appNew'">New</button>
+            <p>Selected Component: {{ selectedComponent }}</p>
+              <component :is="selectedComponent">
+                  <p>Default Content</p>
+              </component>
+               <!--<app-quote >
+                  <h2 slot="title">{{ quoteTitle }}</h2>
+                  <p>A wonderful Quote</p>
+               </app-quote>-->
+                <hr>
+
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+    import Author from './components/Author.vue'
+    import New from './components/New.vue'
+    import Quote from './components/Quote.vue'
+
+    export default {
+      data : function () {
+        return {
+          quoteTitle : 'The Quote',
+          selectedComponent : 'appQuote'
+        }
+      },
+      components: {
+        appQuote : Quote,
+        appAuthor : Author,
+        appNew : New
+      }
+    }
+</script>
+
+<style>
+</style>
