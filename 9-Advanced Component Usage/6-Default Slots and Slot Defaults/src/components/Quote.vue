68e3ab4a9625b0a9a7c429850b76384811e89348
pachoyan <pauchorroyanguas@gmail.com>
2017-02-16 22:15:35 +0100

    Slots

---
+++ 9-Advanced Component Usage/6-Default Slots and Slot Defaults/src/components/Quote.vue
@@ -0,0 +1,38 @@
+<template>
+    <div>
+      <div class="title">
+        <slot name="title"></slot>
+        <!--Default content slot if it is not passed info-->
+        <span style="color: #ccc;"><slot name="subtitle">The subtitle</slot></span>
+      </div>
+      <hr>
+      <div>
+        <!--Unnamed slot is the default slot-->
+        <slot></slot>
+      </div>
+    </div>
+</template>
+
+<script>
+
+    export default {
+    }
+</script>
+
+<style scoped>
+  div {
+    border: 1px solid #ccc;
+    box-shadow : 1px 1px 2px black;
+    padding: 30px;
+    margin: 30px auto;
+    text-align : center;
+  }
+  /* In the parent wouldn't work */
+  h2 {
+    color : red;
+  }
+
+  .title {
+    font-style : italic;
+  }
+</style>
