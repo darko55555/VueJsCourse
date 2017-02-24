e5d2211d5482ee93652d1c03382de7fabd553dde
pachoyan <pauchorroyanguas@gmail.com>
2017-02-24 19:50:29 +0100

    Http

---
+++ 15-Connecting to Servers via Http - Using vue-resource/9-Creating Custom Resources/src/main.js
@@ -0,0 +1,21 @@
+import Vue from 'vue'
+import VueResource from  'vue-resource';
+import App from './App.vue'
+
+//npm install --save vue-resource
+Vue.use(VueResource);
+Vue.http.options.root = 'https://famous-mission-120721.firebaseio.com/';
+Vue.http.interceptors.push((request, next) => {
+  console.log(request);
+  if(request.method == 'POST'){
+    request.method = 'PUT';
+  }
+  next(response => {
+    response.json = () => { return { messages: response.body} }
+  });
+});
+
+new Vue({
+  el: '#app',
+  render: h => h(App)
+})
